711f05125d6c98294efea46887073bce1c140507
Deepakraj Murugesan <deepakraj.murugesan@arvindinternet.com>
2021-08-15 22:42:54 +0530

    added sample tableview test cases

---
+++ KeepTruckinAssignmentTests/Models/SearchViewControllerTest.swift
@@ -0,0 +1,53 @@
+//
+//  SearchViewControllerTest.swift
+//  KeepTruckinAssignmentTests
+//
+//  Created by Deepakraj Murugesan on 15/08/21.
+//
+
+import Foundation
+import XCTest
+@testable import KeepTruckinAssignment
+
+class SideMenuViewControllerTest: XCTestCase {
+    
+    var viewControllerUnderTest: SearchViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        self.viewControllerUnderTest = storyboard.instantiateViewController(withIdentifier: "SearchViewController") as? SearchViewController
+        
+        // in view controller, menuItems = ["one", "two", "three"]
+        self.viewControllerUnderTest.loadView()
+        self.viewControllerUnderTest.viewDidLoad()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testHasATableView() {
+        XCTAssertNotNil(viewControllerUnderTest.searchTableView)
+    }
+    
+    func testTableViewHasDelegate() {
+        XCTAssertNotNil(viewControllerUnderTest.searchTableView.delegate)
+    }
+    
+    func testTableViewConfromsToTableViewDelegateProtocol() {
+        XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDelegate.self))
+    }
+    
+    func testTableViewHasDataSource() {
+        XCTAssertNotNil(viewControllerUnderTest.searchTableView.dataSource)
+    }
+    
+    func testTableViewConformsToTableViewDataSourceProtocol() {
+        XCTAssertTrue(viewControllerUnderTest.conforms(to: UITableViewDataSource.self))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:numberOfRowsInSection:))))
+        XCTAssertTrue(viewControllerUnderTest.responds(to: #selector(viewControllerUnderTest.tableView(_:cellForRowAt:))))
+    }
+}
